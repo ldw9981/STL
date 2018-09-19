fa8bfb3ac09534bbf04aaa3e75ec11af88f85d9a
DESKTOP-59A58HT\ldw9981 <ldw9981@gmail.com>
2018-09-19 11:38:59 +0900

    미로 탐색 스택,큐 이용 로직 일부 수정

--- Day12/Project2/Project2/Source.cpp
+++ Day12/Project2/Project2/Source.cpp
@@ -6,11 +6,12 @@
 using namespace std;
 
 #define MAX 10
-enum EDIR { UP=0,RIGHT,DOWN,LEFT} ;
+enum EDIRECTION { UP=0,RIGHT,DOWN,LEFT} ;
+enum EMAZE { PATH=0,WALL,VISITED,BACKTRACE};
 
 int Maze[MAX][MAX] = {
 	{0,0,1,1,0,0,0,1,1,0},
-	{1,0,0,0,0,0,0,1,1,0},
+	{1,0,0,1,0,0,0,1,1,0},
 	{1,0,1,1,0,0,0,1,1,0},
 	{1,0,1,1,1,0,0,1,1,0},
 	{1,0,0,0,0,0,0,1,1,0},
@@ -21,10 +22,7 @@ int Maze[MAX][MAX] = {
 	{1,0,0,0,0,0,0,1,1,0}
 };
 
-#define PATH 0
-#define WALL 1
-#define VISITED 2
-#define BACKTRACE 3
+
 
 class Position
 {
@@ -40,36 +38,76 @@ public:
 
 Position Dir[4];
 
-void PrintMap()
+void pause()
+{
+	while (_getch() != 0);
+}
+
+void PrintMap(const Position& Current)
 {
 	for (int y = 0; y < MAX; y++)
 	{
 		for (int x = 0; x < MAX; x++)
 		{
-			cout << Maze[y][x] << " ";
+			cout << Maze[y][x] << "\t";
 		}
 		cout << endl;
 
 	}	
-	_getch();
+	cout << Current.x << "," << Current.y << endl;
+	pause();
 	system("cls");
 }
 
-void PrintEscape()
+void PrintPath() //스택용
 {
-	for (int y = 0; y < MAX; y++)
+	for (int y = 0; y < MAX; ++y)
 	{
-		for (int x = 0; x < MAX; x++)
+		for (int x = 0; x < MAX; ++x)
 		{
-			cout << Maze[y][x] << " ";
+			if (Maze[y][x] == VISITED)
+			{
+				cout << "(" << x << ", " << y << ")" << endl;
+			}
 		}
-		cout << endl;
-
 	}
-	_getch();
-	system("cls");
 }
 
+void Trace()
+{
+	Position Current(MAX - 1, MAX - 1);
+	stack<Position> Trace;
+	Trace.push(Current);
+
+	bool found = false;
+	while (Current.y != 0 || Current.x != 0)
+	{
+		int Level = Maze[Current.y][Current.x];
+
+		for (int dir = UP; dir <= LEFT; ++dir)
+		{
+			Position Next(Current.x + Dir[dir].x, Current.y + Dir[dir].y);
+			if (Next.x < 0 || Next.y < 0 ||
+				Next.x >= MAX || Next.y >= MAX) //배열을 벗어남
+			{
+				continue;
+			}
+			if (Maze[Next.y][Next.x] == Level - 1)
+			{
+				Trace.push(Next);
+				Current = Next;
+				break;
+			}
+		}
+	}
+
+	while (!Trace.empty())
+	{
+		Position Temp = Trace.top();
+		Trace.pop();
+		cout << "(" << Temp.x << ", " << Temp.y << ")" << endl;
+	}
+}
 
 bool CanMove(Position& Current)
 {
@@ -77,8 +115,9 @@ bool CanMove(Position& Current)
 	//up
 	Next.x = Current.x;
 	Next.y = Current.y - 1;
-	if (Next.y > -1 && Maze[Next.x][Next.y] == PATH)
+	if (Next.y > -1 && Maze[Next.y][Next.x] == PATH)
 	{
+		cout << Current.x << "," << Current.y << " Can Move up" << endl;
 		Current = Next;
 		return true;
 	}
@@ -86,17 +125,19 @@ bool CanMove(Position& Current)
 	//right
 	Next.x = Current.x +1;
 	Next.y = Current.y ;
-	if (Next.x < MAX && Maze[Next.x][Next.y] == PATH)
+	if (Next.x < MAX && Maze[Next.y][Next.x] == PATH)
 	{
-		Current = Next;
+		cout << Current.x << "," << Current.y << " Can Move right" << endl;
+		Current = Next;		
 		return true;
 	}
 
 	//down
 	Next.x = Current.x;
 	Next.y = Current.y+1;
-	if (Next.y < MAX && Maze[Next.x][Next.y] == PATH)
+	if (Next.y < MAX && Maze[Next.y][Next.x] == PATH)
 	{
+		cout << Current.x << "," << Current.y << " Can Move down" << endl;
 		Current = Next;
 		return true;
 	}
@@ -104,18 +145,20 @@ bool CanMove(Position& Current)
 	//left
 	Next.x = Current.x -1;
 	Next.y = Current.y;
-	if (Next.x > -1 && Maze[Next.x][Next.y] == PATH)
+	if (Next.x > -1 && Maze[Next.y][Next.x] == PATH)
 	{
-		Current = Next;
+		cout << Current.x << "," << Current.y << " Can Move left" << endl;
+		Current = Next;		
 		return true;
 	}
+	cout << Current.x  << "," << Current.y << " -> any = Cant" << endl;
 	return false;
 }
 
 bool CanMove2(Position& Current)
 {
 	Position Next ;
-	for ( int dir = EDIR::UP;  dir <= EDIR::LEFT ; dir++)
+	for ( int dir = UP;  dir <= LEFT ; dir++)
 	{
 		Next.x = Current.x + Dir[dir].x;
 		Next.y = Current.y + Dir[dir].y;
@@ -134,32 +177,61 @@ bool CanMove2(Position& Current)
 	return false;
 }
 
+// 탈출 성공인지 리턴
+bool MazeMove(Position Current)
+{	
+	Maze[Current.y][Current.x] = VISITED;
+	PrintMap(Current);
+
+	if (Current.x == (MAX - 1) && Current.y == (MAX - 1))
+	{		
+		cout << "탈출 성공" << endl;
+		PrintMap(Current);
+		return true;
+	}
+
+	Position Next;
+	for (int dir = EDIRECTION::UP; dir <= EDIRECTION::LEFT; dir++)
+	{
+		Next.x = Current.x + Dir[dir].x;
+		Next.y = Current.y + Dir[dir].y;
+
+		if (Next.x < 0 || Next.y < 0 || Next.x >= MAX || Next.y >= MAX)
+			continue;		
+
+		if (Maze[Next.y][Next.x] == PATH)
+		{
+			if (MazeMove(Next))
+				return true;
+		}	
+	}
+	
+	Maze[Current.y][Current.x] = BACKTRACE;
+	PrintMap(Current);	
+	return false;
+}
 void Process1()
 {
-	//up
-	Dir[EDIR::UP].x = 0;
-	Dir[EDIR::UP].y = -1;
-	//right
-	Dir[EDIR::RIGHT].x = 1;
-	Dir[EDIR::RIGHT].y = 0;
-	//down
-	Dir[EDIR::DOWN].x = 0;
-	Dir[EDIR::DOWN].y = 1;
-	//left
-	Dir[EDIR::LEFT].x = -1;
-	Dir[EDIR::LEFT].y = 0;
-
 	Position Current;
-	Current.x = 0;
-	Current.y = 0;
+	bool Exit=false;
+	MazeMove(Current);
 
+	return;
 	stack<Position> EscapeMap;
 	while (true)
 	{
+		// 현재위치 방문표시
 		Maze[Current.y][Current.x] = VISITED;
-
-		if (CanMove(Current))
+		
+		if (Current.x == (MAX - 1) && Current.y == (MAX - 1))
 		{
+			cout << "탈출 성공" << endl;
+			break;
+		}
+
+		// 이동?
+		if (CanMove(Current))
+		{			
 			EscapeMap.push(Current);
 		}
 		else
@@ -169,8 +241,10 @@ void Process1()
 			if (!EscapeMap.empty())	// 되돌아갈길 있나?
 			{
 				Position temp = EscapeMap.top();
+				//cout << Current.x << "," << Current.y << "되돌아감 -> " << temp.x << "," << temp.y << endl;
 				EscapeMap.pop();
 				Maze[Current.y][Current.x] = BACKTRACE;
+				Current = temp;
 			}
 			else
 			{
@@ -178,14 +252,12 @@ void Process1()
 				break;
 			}
 
-			if (Current.x == (MAX - 1) && Current.y == (MAX - 1))
-			{
-				cout << "탈출 성공" << endl;
-				break;
-			}
+
 		}
-		PrintMap();
+		PrintMap(Current);
 	}
+
+
 
 	while (!EscapeMap.empty())
 	{
@@ -195,139 +267,143 @@ void Process1()
 
 		EscapeMap.pop();
 	}
-	_getch();
+	while (_getch() != 0);
 }
 
-void MazeMove(Position& Current, list<Position>& outList)
-{	
-	Maze[Current.y][Current.x] = VISITED;
-	if (Current.x == (MAX - 1) && Current.y == (MAX - 1))
-	{
-		cout << "탈출 성공" << endl;
-		return;
-	}
-
-	Position Next;
-	PrintMap();
-	for (int dir = EDIR::UP; dir <= EDIR::LEFT; dir++)
-	{
-		Next.x = Current.x + Dir[dir].x;
-		Next.y = Current.y + Dir[dir].y;
-
-		if (Next.x < 0 || Next.y < 0 || Next.x >= MAX || Next.y >= MAX)
-		{
-			continue;
-		}
-
-		if (Maze[Next.y][Next.x] == PATH)
-		{			
-			MazeMove(Next, outList);
-		}
-	}
-	Maze[Current.y][Current.x] = BACKTRACE;
-}
-
-bool MazeMove2(Position Current)
+// 이동가능하면 이동하여 VISITED를 남기고 스택을 풀어 이전 위치로 돌아간다.
+bool MazeMoveRecursive(Position Current)
 {
+	// 인덱스 검사
 	if (Current.x < 0 || Current.y < 0 || Current.x >= MAX || Current.y >= MAX)
 	{
 		return false;
 	}
-	else if (Maze[Current.y][Current.x] != PATH)
+	else if (Maze[Current.y][Current.x] != PATH)  // 길인지 검사
 	{
 		return false;
 	}
-	else if (Current.x == (MAX - 1) && Current.y == (MAX - 1))
+	else if (Current.x == (MAX - 1) && Current.y == (MAX - 1)) // 목적지인지검사
 	{
 		Maze[Current.y][Current.x] = VISITED;
+		PrintMap(Current);
 		return true;
 	}
 	else 
 	{
 		Maze[Current.y][Current.x] = VISITED;
-		if (MazeMove2(Position(Current.x, Current.y - 1)) ||
-			MazeMove2(Position(Current.x+1, Current.y)) ||
-			MazeMove2(Position(Current.x, Current.y + 1)) ||
-			MazeMove2(Position(Current.x-1, Current.y)) )
+		PrintMap(Current);
+		if (MazeMoveRecursive(Position(Current.x, Current.y - 1)) ||
+			MazeMoveRecursive(Position(Current.x+1, Current.y)) ||
+			MazeMoveRecursive(Position(Current.x, Current.y + 1)) ||
+			MazeMoveRecursive(Position(Current.x-1, Current.y)) )
 		{
 			return true;
 		}
 		Maze[Current.y][Current.x] = BACKTRACE;
+		PrintMap(Current);
 		return false;
 	}
 }
 
-void ProcessMaze(Position Current)
+void ProcessMazeWithRecursive(Position Current)
 {	
-	MazeMove2(Current);
+	MazeMoveRecursive(Current);
 }
 
 void InitDir()
 {
 	//up
-	Dir[EDIR::UP].x = 0;
-	Dir[EDIR::UP].y = -1;
+	Dir[UP].x = 0;
+	Dir[UP].y = -1;
 	//right
-	Dir[EDIR::RIGHT].x = 1;
-	Dir[EDIR::RIGHT].y = 0;
+	Dir[RIGHT].x = 1;
+	Dir[RIGHT].y = 0;
 	//down
-	Dir[EDIR::DOWN].x = 0;
-	Dir[EDIR::DOWN].y = 1;
+	Dir[DOWN].x = 0;
+	Dir[DOWN].y = 1;
 	//left
-	Dir[EDIR::LEFT].x = -1;
-	Dir[EDIR::LEFT].y = 0;
+	Dir[LEFT].x = -1;
+	Dir[LEFT].y = 0;
 }
 
 bool CanMoveQ(Position& Current, Position& NewPosition, int dir)
 {
 	Position Next;
+	Next.x = Current.x + Dir[dir].x;
+	Next.y = Current.y + Dir[dir].y;
 
-	for (int i = UP; i <= LEFT; ++i)
+	if (Next.x < 0 || Next.y < 0 ||
+		Next.x > MAX || Next.y > MAX) //배열을 벗어남
 	{
-		Next.x = Current.x + Dir[i].x;
-		Next.y = Current.y + Dir[i].y;
+		return false;
+	}
 
-		if (Next.x < 0 || Next.y < 0 ||
-			Next.x > MAX || Next.y > MAX) // 배열 영역 밖을 벗어나면 이동 불가
-		{
-			continue;
-		}
+	if (Maze[Next.y][Next.x] == PATH) //길이 맞으면 true
+	{
+		NewPosition = Next;
+		return true;
+	}
 
-		if (Maze[Next.y][Next.x] == PATH)
+	return false; //길이 아니면
+
+}
+
+void ProcessMazeWithQueue()
+{
+	queue<Position> Q;
+	InitDir();
+	Position Current;
+
+	Q.push(Current);
+	Maze[Current.y][Current.x] = 10;
+	PrintMap(Current);
+	bool found = false;
+	while (!Q.empty() && !found)
+	{
+		Current = Q.front();
+		Q.pop();
+		int Level = Maze[Current.y][Current.x];
+		for (int dir = UP; dir <= LEFT; ++dir)
 		{
-			Current = Next;
-			return true;
+			Position NewPosition;
+			if (CanMoveQ(Current, NewPosition, dir))
+			{
+				Maze[NewPosition.y][NewPosition.x] = Level + 1;
+				PrintMap(NewPosition);
+				if (NewPosition.x == (MAX - 1) && NewPosition.y == (MAX - 1))
+				{
+					cout << "탈출" << endl;
+					found = true;
+					break;
+				}
+
+				Q.push(NewPosition);
+			}
 		}
 	}
 
-	return false;
+	if (!found)
+	{
+		cout << "탈출 불가" << endl;
+	}
 
+	PrintMap(Current);
+	if (found)
+	{
+		Trace();
+	}
+	pause();
 }
+
 
 int main()
 {
 	InitDir();
 
 	Position Current;
+	//MazeMove(Current);
 
-	queue<Position> Q;
-	InitDir();
-	Position Current;
-
-
-	Q.push(Current);
-	Maze[Current.y][Current.x] = 10;
-	bool found = false;
-	while (!Q.empty())
-	{
-		Current = Q.front();
-		Q.pop();
-		int Level = Maze[Current.y][Current.x];
-		for (int dir = UP; dir <= LEFT ; dir++)
-		{
-
-		}
-	}
+	ProcessMazeWithQueue();
 
 
 
