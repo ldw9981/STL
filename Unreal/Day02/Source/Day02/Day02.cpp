4d4eb1ee7da872dc2096d53a25df6bd9f5072d4b
ldw9981 <ldw9981@gmail.com>
2018-10-23 20:10:10 +0900

    day2

---
+++ Unreal/Day02/Source/Day02/Day02.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Day02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Day02, "Day02" );
