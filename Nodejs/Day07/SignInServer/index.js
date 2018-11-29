e5c1ac03c82820f96ff03a76faf2c627cbabfe9f
ldw9981 <ldw9981@gmail.com>
2018-11-29 18:51:36 +0900

    쿠키,로그인 테스트

---
+++ Nodejs/Day07/SignInServer/index.js
@@ -0,0 +1,26 @@
+// 모듈 추출.
+var server = require('./server');
+var requestHandler = require('./requestHandler');
+var database = require('./database');
+
+// 핸들 객체 생성자.
+function HandleCreator(method, path, callback) {
+    this.method = method;
+    this.path = path;
+    this.callback = callback;
+}
+
+// 핸들 객체 생성.
+var handle = [
+    new HandleCreator('get', '/register', requestHandler.showRegisterForm),
+    new HandleCreator('get', '/', requestHandler.start),
+    new HandleCreator('get', '/login', requestHandler.showLoginForm),
+    new HandleCreator('post', '/register', requestHandler.register),
+    new HandleCreator('post', '/login', requestHandler.login),
+]
+
+// 디비 연결.
+requestHandler.connectDB(database);
+
+// 서버 시작.
+server.serverStart(handle);
