ca16a88eb59ce58aea105bbbf3b49365a4f35035
DESKTOP-59A58HT\ldw9981 <ldw9981@gmail.com>
2018-12-23 00:28:48 +0900

    RandomItem 생성 동기화

---
+++ Unreal/ProjectBD/Source/ProjectBD/Items/RandomItemPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "RandomItemPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBD_API ARandomItemPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
---
+++ Unreal/ProjectBD/Source/ProjectBD/Items/RandomItemSpawner.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RandomItemSpawner.h"
+#include "Kismet/GameplayStatics.h"
+#include "Items/RandomItemPoint.h"
+#include "Engine/World.h"
+#include "MasterItem.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "UnrealNetwork.h"
+
+// Sets default values
+ARandomItemSpawner::ARandomItemSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	bReplicates = true;
+}
+
+// Called when the game starts or when spawned
+void ARandomItemSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (UKismetSystemLibrary::IsServer(GetWorld()))
+	{
+		TSubclassOf<ARandomItemPoint> RandomItemPointClassType = ARandomItemPoint::StaticClass();
+		TArray<AActor*> Results;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), RandomItemPointClassType, Results);
+
+		for (auto it : Results)
+		{
+			int ItemIndex = FMath::RandRange(1, 6) * 10;
+			RandomItems.Add(ItemIndex);
+		}
+		Spwan_OnRep();
+	}
+}
+
+// Called every frame
+void ARandomItemSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARandomItemSpawner::Spwan_OnRep()
+{
+	TSubclassOf<ARandomItemPoint> RandomItemPointClassType = ARandomItemPoint::StaticClass();
+	TArray<AActor*> Results;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), RandomItemPointClassType, Results);
+
+	FVector SourceLocation = GetActorLocation();
+	Results.Sort([SourceLocation](const AActor& A, const AActor& B)
+	{
+		float DistanceA = FVector::DistSquared(SourceLocation, A.GetActorLocation());
+		float DistanceB = FVector::DistSquared(SourceLocation, B.GetActorLocation());
+
+		return DistanceA > DistanceB;
+	});
+
+	for (int Index = 0; Index< Results.Num();Index++)
+	{
+
+		
+		TSubclassOf<AMasterItem> MasterItemClassType = AMasterItem::StaticClass();
+		FActorSpawnParameters SpawnParms;
+		AMasterItem* Item = GetWorld()->SpawnActor<AMasterItem>(MasterItemClassType, SpawnParms);
+		Item->SetActorTransform(Results[Index]->GetActorTransform());
+		Item->SetItem(RandomItems[Index]);
+	}
+}
+
+void ARandomItemSpawner::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(ARandomItemSpawner, RandomItems, COND_InitialOnly);	//COND_InitialOnly - This property will only attempt to send on the initial bunch
+}
+
